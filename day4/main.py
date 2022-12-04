82ba74ee048c10b90771bf5e534f415174943b36
Alberto Azzari <alberto.azzari@univr.it>
2022-12-04 10:54:32 +0200

    fix int conversion on day4!

--- day4/main.py
+++ day4/main.py
@@ -1,16 +1,16 @@
 
 def fully_containing(start_elf1, end_elf1, start_elf2, end_elf2):
-    if int(start_elf1) >= int(start_elf2) and int(end_elf1) <= int(end_elf2):
+    if start_elf1 >= start_elf2 and end_elf1 <= end_elf2:
         return True
-    elif int(start_elf2) >= int(start_elf1) and int(end_elf2) <= int(end_elf1):
+    elif start_elf2 >= start_elf1 and end_elf2 <= end_elf1:
         return True
     else:
         return False
 
 
 def overlapping(start_elf1, end_elf1, start_elf2, end_elf2):
-    xs = set(range(int(start_elf1), int(end_elf1)+1, 1))
-    return len(xs.intersection(range(int(start_elf2), int(end_elf2)+1, 1))) > 0
+    xs = set(range(start_elf1, end_elf1+1, 1))
+    return len(xs.intersection(range(start_elf2, end_elf2+1, 1))) > 0
 
 
 if __name__ == "__main__":
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     overlap = []
     for i in range(len(lines)):
         elf1, elf2 = lines[i].strip().split(',')
-        start_elf1, end_elf1 = elf1.split('-')
-        start_elf2, end_elf2 = elf2.split('-')
+        start_elf1, end_elf1 = [int(x) for x in elf1.split('-')]
+        start_elf2, end_elf2 = [int(x) for x in elf2.split('-')]
         fully_contained.append(fully_containing(start_elf1, end_elf1, start_elf2, end_elf2))
         overlap.append(overlapping(start_elf1, end_elf1, start_elf2, end_elf2))
     print(sum(fully_contained))
